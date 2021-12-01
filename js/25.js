5580e05e8c3f74597f4cdea147a2a006b79ed8ba
Paloma Lozano <palomalogu9@gmail.com>
2021-12-01 19:03:26 +0100

    try catch for error purposes

---
+++ js/25.js
@@ -0,0 +1,11 @@
+//por ejemplo si un pago se pudo completar o no, donde la aplicación siga funcionando pero nos salga el mensaje de error
+
+const num1 = 20;
+const num2 = 30;
+console.log(num1);
+
+try {
+  console.log(num2);
+} catch (error) {
+  console.log(error);
+}
