f89ca4bc499fe140cea6be1278eb8df59cca3dac
Paloma Lozano <palomalogu9@gmail.com>
2021-12-02 10:08:05 +0100

    async await in javascript

--- js/28.js
+++ js/28.js
@@ -10,10 +10,23 @@ function descargarClientes() {
   });
 }
 
-setTimeout(function () {
-  console.log('set time out');
-}, 5000);
+//función asíncrona, async es la promesa lo que dice que va a devolver, await es esperar a que resuelva la promesa mientras se están ejecutando otras tareas de forma normal
 
-setInterval(function () {
-  console.log('set interval');
-}, 5000);
+async function app() {
+  try {
+    const resultado = await descargarClientes();
+    console.log(resultado);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+app();
+
+// setTimeout(function () {
+//   console.log('set time out');
+// }, 5000);
+
+// setInterval(function () {
+//   console.log('set interval');
+// }, 5000);
